a0e4c16124680743cdf41882c439ff6ced4ef915
Christopher Phelefu <cphelefu@gmail.com>
2024-09-24 16:20:19 -0400

    moving tabs to the left\

--- src/components/ConfigTextEditor.tsx
+++ src/components/ConfigTextEditor.tsx
@@ -1,8 +1,9 @@
 import { useContext, useEffect, useRef, useState } from 'react';
 import { CGPVContext } from '../providers/cgpvContextProvider/CGPVContextProvider';
-import { Box, Button } from '@mui/material';
-import { ConfigSaveUploadButtons } from './ConfigSaveUploadButtons';
+import { Box, Button, Modal } from '@mui/material';
 import { useSnackbar } from '@/providers/snackbarProvider';
+import DataObjectIcon from '@mui/icons-material/DataObject';
+import { CopyToClipboardButton } from './CopyToClipboardButton';
 
 export function ConfigTextEditor() {
   const cgpvContext = useContext(CGPVContext);
@@ -20,13 +21,18 @@ export function ConfigTextEditor() {
   const [numberOfLines, setNumberOfLines] = useState<number>(0);
   const [isValidJson, setIsValidJson] = useState<boolean>(false);
   const [isEditorTouched, setIsEditorTouched] = useState<boolean>(false);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
 
   useEffect(() => {
+    resetEditorText();
+  }, [configJson]);
+
+  const resetEditorText = () => {
     const jsonObj = configJson || {};
     const jsonTxt = JSON.stringify(jsonObj, null, 4);
     setEditorText(jsonTxt);
     setIsEditorTouched(false);
-  }, [configJson]);
+  };
 
   useEffect(() => {
     const numOfLines = editorText.split(/\r\n|\r|\n/).length;
@@ -58,41 +64,80 @@ export function ConfigTextEditor() {
     return Array.from({ length: n }, (_, index) => index);
   }
 
+  const handleOpenModal = function () {
+    setModalIsOpen(true);
+  }
+
+  const handleCloseModal = function () {
+    if (isEditorTouched) {
+      resetEditorText();
+    } else {
+      setModalIsOpen(false);
+    }
+  }
+
+  const modalContentStyle = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: { xs: '90%', sm: '90%', md: '80%', lg: '75%' },
+    bgcolor: 'background.paper',
+    border: '2px solid #ccc',
+    borderRadius: 4,
+    boxShadow: 24,
+    display: 'flex', 
+    flexDirection: 'column',
+    p: 1,
+  };
+
   return (
-    <Box sx={{ display: 'flex', flexDirection: 'column', position:'relative' }}>
-      <Box className="config-editor" sx={{position: 'relative'}}>
-        <Box sx={{position: 'absolute', top: 5, left: 10}}>
-          <Button variant="contained" color="primary" size="small" onClick={handleApplyStateToConfigFile}>
-            Apply State to Config File
-          </Button>
+    <>
+      <Button component="label" variant="contained" color="primary" size="small" onClick={handleOpenModal} startIcon={<DataObjectIcon />}>
+        Edit JSON
+      </Button>
+      <Modal
+        open={modalIsOpen}
+        onClose={handleCloseModal}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={modalContentStyle}>
+          <Box className="config-editor" sx={{ position: 'relative' }}>
+            <Box sx={{ position: 'absolute', top: 5, left: 10 }}>
+              <Button variant="contained" color="primary" size="small" onClick={handleApplyStateToConfigFile}>
+                Apply State to Config File
+              </Button>
+            </Box>
+            <Box sx={{ position: 'absolute', top: 5, right: 10 }}>
+              <CopyToClipboardButton textToCopy={editorText} />
+            </Box>
+            <div className="line-numbers">
+              {generateArray(numberOfLines).map((lineNumber) => (
+                <span key={lineNumber}></span>
+              ))}
+            </div>
+            <textarea
+              id="configGeoview"
+              name="configuration"
+              value={editorText}
+              onChange={onTextareaChange}
+              rows={30}
+              cols={150}
+              ref={textEditorRef}
+              spellCheck="false"
+            ></textarea>
+          </Box>
+          <Box sx={{ display: 'flex', justifyContent: 'center', mt: 2, gap: 2 }}>
+            <Button variant="contained" color="primary" onClick={validateText} disabled={!isEditorTouched}>
+              Validate
+            </Button>
+            <Button variant="contained" color="primary" onClick={createMap} disabled={!isValidJson}>
+              Create Map
+            </Button>
+          </Box>
         </Box>
-        <Box sx={{position: 'absolute', top: 5, right: 10}}>
-          <ConfigSaveUploadButtons showCopy={true} />
-        </Box>
-        <div className="line-numbers">
-          {generateArray(numberOfLines).map((lineNumber) => (
-            <span key={lineNumber}></span>
-          ))}
-        </div>
-        <textarea
-          id="configGeoview"
-          name="configuration"
-          value={editorText}
-          onChange={onTextareaChange}
-          rows={30}
-          cols={150}
-          ref={textEditorRef}
-          spellCheck="false"
-        ></textarea>
-      </Box>
-      <Box sx={{ display: 'flex', justifyContent: 'center', mt: 2, gap: 2 }}>
-        <Button variant="contained" color="primary" onClick={validateText} disabled={!isEditorTouched}>
-          Validate
-        </Button>
-        <Button variant="contained" color="primary" onClick={createMap} disabled={!isValidJson}>
-          Create Map
-        </Button>
-      </Box>
-    </Box>
+      </Modal>
+    </>
   );
 }
--- src/components/LegendLayerStatusTable.tsx
+++ src/components/LegendLayerStatusTable.tsx
@@ -1,47 +1,34 @@
 import { useContext } from 'react';
 import { CGPVContext } from '../providers/cgpvContextProvider/CGPVContextProvider';
-import { Box, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+import { Box, Divider, List, ListItem, ListItemText } from '@mui/material';
 
 
 export function LegendLayerStatusTable() {
-    const cgpvContext = useContext(CGPVContext);
-  
-    if (!cgpvContext) {
-      throw new Error('CGPVContent must be used within a CGPVProvider');
-    }
-  
-    const { legendLayerStatusList } = cgpvContext;
+  const cgpvContext = useContext(CGPVContext);
+
+  if (!cgpvContext) {
+    throw new Error('CGPVContent must be used within a CGPVProvider');
+  }
+
+  const { legendLayerStatusList } = cgpvContext;
 
 
-    return (
-        <Box sx={{ p: 2 }}>
-          <h2>Legend Layer Status</h2>
-    
-          <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 550 }} aria-label="simple table">
-              <TableHead sx={{ fontWeight: 'bold' }}>
-                <TableRow>
-                  <TableCell>Layer Name</TableCell>
-                  <TableCell>Status</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {legendLayerStatusList.map((row, index) => (
-                  <TableRow
-                    key={`$layer_index_${index}`}
-                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                  >
-                    <TableCell component="th" scope="row">
-                      {row.layerName}
-                    </TableCell>
-                    <TableCell>{row?.status}</TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </Box>
-      );
-    
+  return (
+    <Box sx={{ p: 2 }}>
+      <h2>Legend Layer Status</h2>
+
+      <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+        {legendLayerStatusList.map((row, index) => (
+          <>
+            <ListItem key={`$legend_layer_status_index_${index}`}>
+              <ListItemText primary={row.layerName} secondary={row?.status} />
+            </ListItem>
+            <Divider component="li" />
+          </>
+        ))}
+      </List>
+    </Box>
+  );
+
 
 };
